4a83727dc26a4d43da4e29bde2d9a76fbddc332b
Rannver <562763650@qq.com>
2018-03-26 19:48:29 +0800

    add requset permission

--- app/src/main/java/rannver/ardemo/ui/MainActivity.java
+++ app/src/main/java/rannver/ardemo/ui/MainActivity.java
@@ -7,15 +7,17 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,14 +29,14 @@ import rannver.ardemo.model.ModelGLView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btu_camera)
-    FloatingActionButton btuCamera;
-    @BindView(R.id.btu_start)
-    FloatingActionButton btuStart;
     @BindView(R.id.preview)
     FrameLayout preview;
+    @BindView(R.id.btu_camera)
+    ImageButton btuCamera;
+    @BindView(R.id.btu_start)
+    ImageButton btuStart;
     @BindView(R.id.btu_end)
-    FloatingActionButton btuEnd;
+    ImageButton btuEnd;
 
     private String key = "gjWuewAzkutlvqSyu23DCl5tzd5QeUSb29gQTBEHicQtDXhV0cq0hJidkQEyxHeWKpQmcDUHZG0Wx73gKiQuxZ7g7dEV7GOxEW2mtprAHx0Df3WxGAWUL0jeOYrgs68AjTm8hfpmXEiMcYufRYsIgEnmoj28OZzusqaJicjt1NAE30xF3Zp6H7CWgjnyyWiIMiUF9xyg";
     private GLView glView;
@@ -59,9 +61,9 @@ public class MainActivity extends AppCompatActivity {
         requestCameraPermission(new PermissionCallback() {
             @Override
             public void onSuccess() {
-                if (flag.equals("model2")){
+                if (flag.equals("model2")) {
                     ((ViewGroup) findViewById(R.id.preview)).addView(modelGLView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                }else{
+                } else {
                     ((ViewGroup) findViewById(R.id.preview)).addView(glView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 }
             }
@@ -79,14 +81,14 @@ public class MainActivity extends AppCompatActivity {
             flag = intent.getStringExtra("flag");
         }
 
-        if (flag != null&&(!flag.equals("model2"))) {
+        if (flag != null && (!flag.equals("model2"))) {
             glView = new GLView(this, plyPath, flag);
         }
-        if (flag .equals("model2")) {
-            modelGLView = new ModelGLView(this,plyPath);
+        if (flag.equals("model2")) {
+            modelGLView = new ModelGLView(this, plyPath);
         }
 
-        if (flag .equals("ar")||flag.equals("model2")) {
+        if (flag.equals("ar") || flag.equals("model2")) {
             btuStart.setVisibility(View.GONE);
         }
     }
@@ -123,13 +125,29 @@ public class MainActivity extends AppCompatActivity {
 
     @TargetApi(23)
     private void requestCameraPermission(PermissionCallback callback) {
+
+        String[] permissions = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
+        List<String> permissionList = new ArrayList<>();
+
         if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                int requestCode = permissionRequestCodeSerial;
-                permissionRequestCodeSerial += 1;
-                permissionCallbacks.put(requestCode, callback);
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, requestCode);
-            } else {
+//            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                int requestCode = permissionRequestCodeSerial;
+//                permissionRequestCodeSerial += 1;
+//                permissionCallbacks.put(requestCode, callback);
+//                requestPermissions(permissions, requestCode);
+//            } else {
+//                callback.onSuccess();
+//            }
+            for (int i = 0;i< permissions.length;i++){
+                if (checkSelfPermission(permissions[i])!= PackageManager.PERMISSION_GRANTED){
+                    permissionList.add(permissions[i]);
+                }
+            }
+            if (!permissionList.isEmpty()){
+                String[] needPermission = permissionList.toArray(new String[permissionList.size()]);
+                permissionCallbacks.put(1,callback);
+                requestPermissions(needPermission,1);
+            }else {
                 callback.onSuccess();
             }
         } else {
@@ -162,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         if (glView != null) {
             glView.onResume();
         }
-        if (modelGLView!=null){
+        if (modelGLView != null) {
             modelGLView.onResume();
         }
     }
@@ -172,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
         if (glView != null) {
             glView.onPause();
         }
-        if (modelGLView!=null){
+        if (modelGLView != null) {
             modelGLView.onPause();
         }
         super.onPause();
